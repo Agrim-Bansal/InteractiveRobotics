654c816a766c2c4c52bcbf93b367b6c665bdb1c9
Agrim Bansal <agrimbansal12@gmail.com>
2025-02-12 17:50:36 +0530

    Minor changes in frontend

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
-import { Montserrat } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 
-const montserrat = Montserrat({
-  variable: "--font-montserrat",
+const inter = Inter({
+  variable: "--font-inter",
   subsets: ["latin"],
   weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
   display: "swap",
@@ -23,7 +23,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${montserrat.variable} antialiased`}
+        className={`${inter.variable} antialiased h-screen`}
       >
         {children}
       </body>
